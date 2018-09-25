b5032b75c81c7433b34c45ab4d41ffd966d37b62
Julia Dibo <judibo@Julias-MacBook-Air.local>
2018-09-25 13:08:19 -0700

    Create win logic

--- js/main.js
+++ js/main.js
@@ -17,6 +17,45 @@ var board = document.getElementById('board');
 board.addEventListener('click', clickSlot);
 
 /*----- functions -----*/
+
+function checkForWin() {
+    //check vertical
+    for (var colIdx = 0; colIdx <= 6; colIdx++) {
+        for ( var rowIdx = 0; rowIdx <= 5; rowIdx++) {
+            Math.abs(gameboard[colIdx][rowIdx] + gameboard[colIdx][rowIdx+1] + gameboard[colIdx][rowIdx+2] + gameboard[colIdx][rowIdx+3]) === 4 ? alert(`Winner is: ${players[turn]}`) : null;
+        };
+    };
+    //check horizontal
+    for (var colIdx = 0; colIdx <= 3; colIdx++) {
+        for ( var rowIdx = 0; rowIdx <= 5; rowIdx++) {
+            // totalSumRight === 4 ? alert (`Winner is: ${players[turn]}`) : null;
+            Math.abs(gameboard[colIdx][rowIdx] + gameboard[colIdx+1][rowIdx] + gameboard[colIdx+2][rowIdx] + gameboard[colIdx+3][rowIdx]) === 4 ? alert(`Winner is: ${players[turn]}`) : null;
+        };
+    };
+    //check diagonal up
+    for (var colIdx = 0; colIdx <= 3; colIdx++) {
+        for ( var rowIdx = 0; rowIdx <= 5; rowIdx++) {
+            Math.abs(gameboard[colIdx][rowIdx] + gameboard[colIdx+1][rowIdx+1] + gameboard[colIdx+2][rowIdx+2] + gameboard[colIdx+3][rowIdx+3]) === 4 ? alert(`Winner is: ${players[turn]}`) : null;
+        };
+    };
+};
+
+            // var sumUpRight = (gameboard[colIdx][rowIdx] + gameboard[colIdx+1][rowIdx+1] + gameboard[colIdx+2][rowIdx+2] + gameboard[colIdx+3][rowIdx+3]);
+            // var totalSumUpRight = Math.abs(sumUpRight);
+            // totalSumUpRight === 4 ? alert (`Winner is: ${players[turn]}`) : null;
+
+
+
+function getWinnner() {
+    for (var colIdx = 0; colIdx < gameboard.length; colIdx++) {
+        for ( var rowIdx = 0; rowIdx < gameboard[colIdx].length; rowIdx++) {
+            if (gameboard[colIdx][rowIdx] === null) break;
+            if (winner) break;
+        };
+        if (winner) break;
+    };        
+};      
+
 function render() {
     gameboard.forEach( function(col, colIdx) {
         col.forEach( function(cell, rowIdx ) {
@@ -30,14 +69,13 @@ function clickSlot(evt) {
     var target = evt.target;
     if (target.tagName !== 'TD') return;
     var col = parseInt(evt.target.id.charAt(1));
-    if (!gameboard[col].includes(null)) {
-        alert('try another column');
-    };
+    if (!gameboard[col].includes(null));
     var row = gameboard[col].indexOf(null);
     gameboard[col][row] = turn;
+    checkForWin();
+    winner = getWinnner();
     turn *= -1;
     render();
-    getWinner();
 };
 
 function initialize() {
@@ -48,6 +86,7 @@ function initialize() {
         };
     };
     turn = 1;
+    winner = null;
 };
 
 initialize();
